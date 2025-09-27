41782d786c21996b8e414a5874cb24ba290fd7c0
叮咚叮咚 <xiaodong1919810@163.com>
2025-09-27 14:34:34 +0800

    feat: first page

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 import flask
 from flask import Flask, render_template, request, redirect, url_for
+import base64, time, json, re, os, uuid, threading, requests, smtplib, sys
+import http.client
+
+app = Flask(__name__) # 创建 Flask 应用
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
